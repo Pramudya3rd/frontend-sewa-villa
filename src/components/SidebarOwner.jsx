000ac53698b5db31380252557d0a3cf7d907bc9c
dianranie <dianranie>
2025-06-02 15:11:19 +0700

    add logo at owner page

--- src/components/SidebarOwner.jsx
+++ src/components/SidebarOwner.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../assets/logo-kamar-tamu.png';
 import "../styles/SideBar.css";
 
 const SidebarOwner = ({ setActiveMenu }) => {
   return (
-    <div className="sidebar">
-      <div className="logo">LOGO</div>
+    <div className="sidebar" style={{ alignItems: 'center', padding: 0 }}>
+      {/* LOGO */}
+      <Link to="/" style={{ padding: 0, margin: 0 }}>
+        <img src={logo} alt="Logo" style={{ height: '75px' }} />
+      </Link>
 
-      <button className="sidebar-btn" onClick={() => setActiveMenu("dashboard")}>
-        DASHBOARD
-      </button>
+      {/* MENU */}
+      <div className="sidebar-menu" style={{ marginTop: '30px', width: '100%' }}>
+        <button className="sidebar-btn" onClick={() => setActiveMenu("dashboard")}>
+          DASHBOARD
+        </button>
 
-      <button className="sidebar-btn" onClick={() => setActiveMenu("villa")}>
-        MY VILLA
-      </button>
+        <button className="sidebar-btn" onClick={() => setActiveMenu("villa")}>
+          MY VILLA
+        </button>
 
-      <button className="sidebar-btn" onClick={() => setActiveMenu("booking")}>
-        BOOKING
-      </button>
+        <button className="sidebar-btn" onClick={() => setActiveMenu("booking")}>
+          BOOKING
+        </button>
+      </div>
     </div>
   );
 };
